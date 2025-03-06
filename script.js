0c74912a8fad0aa441e35f78ca26e3872e29e240
01KBrooker <Xcom1661@gmail.com>
2025-03-05 22:45:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 //create a array of sounds
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong','game over'];
 
 sounds.forEach((sound) => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
     btn.innerText = sound;
+    //add a listener to a button
+    btn.addEventListener('click', () => {
+       stopSounds();
+        document.getElementById(sound).play();
+    });
     document.getElementById('buttons').appendChild(btn);
 });
 
